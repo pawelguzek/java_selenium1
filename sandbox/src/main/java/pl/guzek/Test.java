2e08e5addf2f21ac018985cae8ff43b0ec56b04b
pawel.guzek <pawel.guzek@fru.pl>
2017-03-27 00:02:48 +0200

    TestNG added from maven repo

---
+++ sandbox/src/main/java/pl/guzek/Test.java
@@ -0,0 +1,16 @@
+package pl.guzek;
+
+public class Test{
+
+  public static void main(String[] args){
+
+  	String word1 = "Hello";
+  	String separator = ", ";
+  	String word2 = "world";
+
+  	int num1 = 2;
+  	double num2 = 3.0;
+
+	System.out.println(word1 +separator+ word2);
+	}
+}
